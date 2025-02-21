34f65eecad39594449e947a210fb9cce627890b0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-22 05:56:41 +0800

    [PERF] affine SF/render engine to big cores
    Change-Id: I969fba2bc3bbbeb3ab2df83b08ad0840ae3c7773
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -942,6 +942,37 @@ void SurfaceFlinger::init() FTL_FAKE_GUARD(kMainThreadContext) {
         ALOGW("Failed to set main task profile");
     }
 
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+
+    std::vector<int32_t> big_cores;
+
+    auto parseCpusets = [](const std::string& cpuset_str, std::vector<int32_t>& cpus) {
+        std::istringstream ss(cpuset_str);
+        std::string token;
+        while (std::getline(ss, token, ',')) {
+            char* endptr;
+            long cpu = std::strtol(token.c_str(), &endptr, 10);
+            if (*endptr == '\0' && cpu >= 0) {
+                cpus.push_back(static_cast<int32_t>(cpu));
+            } else {
+                ALOGW("Invalid CPU core value: %s", token.c_str());
+            }
+        }
+    };
+
+    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_big", "4,5,6,7"), big_cores);
+
+    for (int core : big_cores) {
+        CPU_SET(core, &cpuset);
+    }
+
+    if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) != 0) {
+        ALOGW("Failed to set SurfaceFlinger CPU affinity to big cores!");
+    } else {
+        ALOGI("Successfully set SurfaceFlinger CPU affinity to big cores!");
+    }
+
     mCompositionEngine->setTimeStats(mTimeStats);
 
     mHWComposer = getFactory().createHWComposer(mHwcServiceName);
