51d1127b8daeb40e53d4f4a396af4db222bae66e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-06 11:30:27 +0800

    [PERF] main_surfaceflinger: affine to big cores
    Change-Id: I01a21153d39c4ae301785ffb9157a334c3fe9098
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/surfaceflinger/main_surfaceflinger.cpp
+++ services/surfaceflinger/main_surfaceflinger.cpp
@@ -22,6 +22,7 @@
 
 #include <sched.h>
 
+#include <android-base/properties.h>
 #include <android/frameworks/displayservice/1.0/IDisplayService.h>
 #include <android/hardware/configstore/1.0/ISurfaceFlingerConfigs.h>
 #include <android/hardware/graphics/allocator/2.0/IAllocator.h>
@@ -138,6 +139,37 @@ int main() {
     setpriority(PRIO_PROCESS, 0, PRIORITY_URGENT_DISPLAY);
 
     set_sched_policy(0, SP_TOP_APP);
+
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+
+    std::vector<int32_t> big_cores;
+
+    auto parseCpusets = [](const std::string& cpuset_str, std::vector<int32_t>& cpus) {
+        std::istringstream ss(cpuset_str);
+        std::string token;
+        while (std::getline(ss, token, ',')) {
+            char* endptr;
+            long cpu = std::strtol(token.c_str(), &endptr, 10);
+            if (*endptr == '\0' && cpu >= 0) {
+                cpus.push_back(static_cast<int32_t>(cpu));
+            } else {
+                ALOGW("Invalid CPU core value: %s", token.c_str());
+            }
+        }
+    };
+
+    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_big", "4,5,6,7"), big_cores);
+
+    for (int core : big_cores) {
+        CPU_SET(core, &cpuset);
+    }
+
+    if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) != 0) {
+        ALOGW("Failed to set SurfaceFlinger CPU affinity to big cores!");
+    } else {
+        ALOGI("Successfully set SurfaceFlinger CPU affinity to big cores!");
+    }
 
     // initialize before clients can connect
     flinger->init();
